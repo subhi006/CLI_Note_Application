3741ca3df2428a9e59e1b33d74ff301a2e12caca
Subhi0600 <Subhi0600@gmail.com>
2024-12-16 03:05:18 +0530

    start poin of the application first run it

---
+++ start_app.rb
@@ -0,0 +1,41 @@
+require_relative "Register.rb"
+require_relative "Login.rb"
+
+class Start
+    include Register
+    include Login
+    def menu
+        while(true)
+            puts "
+            Please choose an option:
+            1. Register
+            2. Login
+            3. Exit"
+            print "Enter option number:"
+            num = gets.chomp.to_i
+            case num
+            when 1
+                print "Enter username:"
+                name=(gets.chomp)
+                print "Enter password:"
+                password=(gets.chomp)
+                user = Register::User.new(name,password)
+                user.user_Register
+            when 2
+                print "Enter username:"
+                name=gets.chomp
+                print "Enter password:"
+                pass=gets.chomp
+                userinfo = Login::User_login.new(name,pass)
+                userinfo.check
+            when 3 
+              puts "Goodbye!"
+              return
+            end
+            gets 
+        end
+    end
+end
+
+start = Start.new 
+start.menu

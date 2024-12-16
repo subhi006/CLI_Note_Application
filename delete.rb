44e03796a44aa4a415338ea0ff98ed29990c5475
Subhi0600 <Subhi0600@gmail.com>
2024-12-16 12:58:13 +0530

    handling the empty note case and changing some variable or instance  name

--- delete.rb
+++ delete.rb
@@ -5,24 +5,32 @@ module Delete
         def initialize(id,user)
             @userName = user['name']
             @note = user['notes']
-            @id = id.to_i
-            @data = UData::Info.new.user_information
+            @id = id.to_i 
+        end
+        def dNote
+                @data = UData::Info.new.user_information
                 @data.each do |user|
                     if(user['name']==@userName)
-                        ud = user['notes']
-                        ud.each do |u|
-                            if u["index"].eql?(@id)
-                               u.clear
-                            elsif u["index"]> @id
-                               u["index"]=u["index"]-1
-                            end
-                        end
-                       ud.delete_if{|d| d.empty?}
-                    end
+                        userNote = user['notes']
+                        if userNote.empty?
+                            puts "There is no Note!"
+                            return
+                        else
+                            userNote.each do |note|
+                                if note["index"].eql?(@id)
+                                   note.clear
+                                elsif note["index"]> @id
+                                  note["index"]=note["index"]-1
+                                end
+                             end
+                         end
+                         userNote.delete_if{|d| d.empty?}
+                     end
+                    File.open("User_info.json",'w') do |file|
+                        file.write(JSON.pretty_generate(@data))
+                     end  
                 end
-            File.open("User_info.json",'w') do |file|
-                file.write(JSON.pretty_generate(@data))
-             end  
+             puts "Note Deleted successfully!"
         end
     end
 end
